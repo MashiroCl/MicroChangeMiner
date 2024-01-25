6bbb7ad7b434c0af598804973b66f2139b7d830b
Lei Chen <mashirocl@gmail.com>
2024-01-25 17:01:58 +0100

    update: remove useless main method

--- src/main/java/org/mashirocl/util/LinkAttacher.java
+++ src/main/java/org/mashirocl/util/LinkAttacher.java
@@ -1,7 +1,6 @@
 package org.mashirocl.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.core.internal.registry.osgi.OSGIUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,13 +14,6 @@ import java.util.Arrays;
  */
 @Slf4j
 public class LinkAttacher {
-
-    public static void main(String [] args){
-
-        String link = LinkAttacher.attachLink("2a151b090cf70e012d61b37d1f4dc1039012d173",
-                LinkAttacher.searchLink("my-refactoring-toy-example"));
-    }
-
     static final ClassLoader classLoader = LinkAttacher.class.getClassLoader();
     static final String linkResource = "githublinks.txt";
 
