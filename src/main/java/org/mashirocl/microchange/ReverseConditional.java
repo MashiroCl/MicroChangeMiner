26b4ec6657ea65edf0322f4ee03f75c1a534e19b
Lei Chen <mashirocl@gmail.com>
2024-02-27 10:58:30 +0100

    feat: can detect add ! to prefix-expression or remove ! from prefix-expression

--- src/main/java/org/mashirocl/microchange/ReverseConditional.java
+++ src/main/java/org/mashirocl/microchange/ReverseConditional.java
@@ -4,6 +4,7 @@ import com.github.gumtreediff.actions.model.Action;
 import com.github.gumtreediff.actions.model.Update;
 import com.github.gumtreediff.tree.Tree;
 import com.google.common.collect.Range;
+import lombok.extern.slf4j.Slf4j;
 import org.mashirocl.editscript.EditScriptStorer;
 import org.mashirocl.location.RangeOperations;
 
@@ -15,6 +16,7 @@ import java.util.Map;
  * @author mashirocl@gmail.com
  * @since 2024/01/23 21:51
  */
+@Slf4j
 public class ReverseConditional implements MicroChangePattern{
 
     public float matchConditionGumTreeCL(Action action,  Map<Tree, Tree> mappings){
@@ -61,8 +63,8 @@ public class ReverseConditional implements MicroChangePattern{
 //            System.out.println("-------------------------");
 //        }
 
+
         return isInsertExclamationInfix(action, mappings)
-                || isInsertExclamationPrefix(action, mappings)
                 || isRemoveExclamationInfix(action, mappings)
                 || isInverseSmallerOrEqualThan(action, mappings)
                 || isInverseSmallerOrEqualThanOrderChange(action, mappings)
@@ -71,7 +73,9 @@ public class ReverseConditional implements MicroChangePattern{
                 || isInverseGreaterOrEqualThan(action, mappings)
                 || isInverseGreaterOrEqualThanOrderChange(action, mappings)
                 || isInverseGreaterThan(action, mappings)
-                || isInverseGreaterThanOrderChange(action, mappings);
+                || isInverseGreaterThanOrderChange(action, mappings)
+                || isAddExclamationPrefix(action,mappings)
+                || isDeleteExclamationPrefix(action, mappings);
     }
 
     @Override
@@ -82,16 +86,44 @@ public class ReverseConditional implements MicroChangePattern{
     @Override
     public SrcDstRange getSrcDstRange(Action action, Map<Tree, Tree> mappings, Map<Tree, List<Action>> nodeActions, EditScriptStorer editScriptStorer) {
         SrcDstRange srcDstRange = new SrcDstRange();
-        srcDstRange.getSrcRange().add(
-                RangeOperations.toLineRange(
-                        RangeOperations.toRange(action.getNode()), editScriptStorer.getSrcCompilationUnit()
-                ));
-        srcDstRange.getDstRange().add(
-                RangeOperations.toLineRange(
-                        RangeOperations.toRange(mappings.get(action.getNode())),editScriptStorer.getDstCompilationUnit()
-                )
-        );
+        //TODO calculate according to the type of action
 
+        switch (action.getName()){
+            case "insert-node":
+                // insert ! in prefix
+                srcDstRange.getDstRange().add(
+                        RangeOperations.toLineRange(
+                                RangeOperations.toRange(action.getNode()),editScriptStorer.getDstCompilationUnit()
+                        )
+                );
+                srcDstRange.getSrcRange().add(
+                        RangeOperations.toLineRange(
+                                RangeOperations.toRange(mappings.get(action.getNode().getParent().getChild(1))),editScriptStorer.getSrcCompilationUnit()
+                        )
+                );
+                break;
+            case "delete-node":
+                // delete ! in prefix
+                srcDstRange.getSrcRange().add(
+                        RangeOperations.toLineRange(
+                                RangeOperations.toRange(action.getNode()), editScriptStorer.getSrcCompilationUnit()
+                        ));
+                srcDstRange.getDstRange().add(
+                        RangeOperations.toLineRange(
+                                RangeOperations.toRange(mappings.get(action.getNode().getParent().getChild(1))), editScriptStorer.getDstCompilationUnit()
+                        ));
+                break;
+            default:
+                srcDstRange.getSrcRange().add(
+                        RangeOperations.toLineRange(
+                                RangeOperations.toRange(action.getNode()), editScriptStorer.getSrcCompilationUnit()
+                        ));
+                srcDstRange.getDstRange().add(
+                        RangeOperations.toLineRange(
+                                RangeOperations.toRange(mappings.get(action.getNode())),editScriptStorer.getDstCompilationUnit()
+                        )
+                );
+        }
         return srcDstRange;
     }
 
@@ -126,6 +158,39 @@ public class ReverseConditional implements MicroChangePattern{
             return isBeforeAfterEqualForInfix(action, mappings) || isBeforeAfterReverseForInfix(action, mappings);
         }
         return false;
+    }
+
+    /**
+     * X ->!X
+     *
+     * @param action
+     * @param mappings
+     * @return
+     */
+    private boolean isAddExclamationPrefix(Action action, Map<Tree, Tree> mappings){
+        return action.getName().equals("insert-node")
+                && action.getNode().getParent().getParent().getType().name.equals("IfStatement")
+                && action.getNode().toString().contains("PREFIX_EXPRESSION_OPERATOR: !")
+                && mappings.containsKey(action.getNode().getParent().getChild(1)) // the expression of the prefix expression on the left side exists
+                && mappings.get(action.getNode().getParent().getChild(1)).getParent().getType().name.equals("IfStatement") // the expression on the left is the child of if
+                && mappings.get(action.getNode().getParent().getChild(1)).isIsomorphicTo(action.getNode().getParent().getChild(1)); // the expression on the left side and right side are equal to each other
+    }
+
+    /**
+     * !X ->X
+     *
+     * @param action
+     * @param mappings
+     * @return
+     */
+    private boolean isDeleteExclamationPrefix(Action action, Map<Tree, Tree> mappings){
+        //TODO:  action.getNode().getParent().getParent() can be a InfixExpression, e.g., if A&B&C -> if A&!B&C
+        return action.getName().equals("delete-node")
+                && action.getNode().getParent().getParent().getType().name.equals("IfStatement")
+                && action.getNode().toString().contains("PREFIX_EXPRESSION_OPERATOR: !")
+                && mappings.containsKey(action.getNode().getParent().getChild(1)) // the expression of the prefix expression still exists
+                && mappings.get(action.getNode().getParent().getChild(1)).getParent().getType().name.equals("IfStatement") // the expression on the left side is the child of a if
+                && mappings.get(action.getNode().getParent().getChild(1)).isIsomorphicTo(action.getNode().getParent().getChild(1)); // the expression before and after is the same
     }
 
     private boolean isBeforeAfterEqualForInfix(Action action, Map<Tree, Tree> mappings){
@@ -165,7 +230,7 @@ public class ReverseConditional implements MicroChangePattern{
             if(action.getNode().getParent().getChildren().size()>1
                     && mappings.containsKey(action.getNode().getParent().getChild(1))
                     && action.getNode().getParent().getChild(1).getType().toString()     // condition before and after are boolean variable/  method invocation
-                    .equals(mappings.get(action.getNode().getParent().getChild(1)).toString())){
+                    .equals(mappings.get(action.getNode().getParent().getChild(1)).toString())){ //TODO: anyway to unify the boolean and method invocation?
                 // the condition is a boolean variable
                 if(action.getNode().getParent().getChild(1).getType().toString().equals("SimpleName")){
                     if(action.getNode().getParent().getChild(1).getLabel()
@@ -176,7 +241,7 @@ public class ReverseConditional implements MicroChangePattern{
 
                 // the condition is a method invocation
                 if(action.getNode().getParent().getChild(1).getType().toString().equals("MethodInvocation")){
-                    return action.getNode().getParent().getChild(1).getChild(1).getLabel()
+                    return action.getNode().getParent().getChild(1).getChild(1).getLabel()  // use getlabel or use to string?
                             .equals(mappings.get(action.getNode().getParent().getChild(1)).getChild(1).getLabel());
 
 //                    System.out.println("in method invocation");
@@ -193,6 +258,8 @@ public class ReverseConditional implements MicroChangePattern{
 
         return false;
     }
+
+
 
     /**
      * X>Y -> X<=Y
