b65d9379fb9fd4455f7a3abc08fc01c338da157d
Lei Chen <mashirocl@gmail.com>
2024-03-19 17:36:40 +0100

    Update: add constructor

--- src/main/java/org/mashirocl/refactoringminer/SideLocation.java
+++ src/main/java/org/mashirocl/refactoringminer/SideLocation.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.eclipse.jgit.diff.DiffEntry;
 
 import java.nio.file.Path;
 
@@ -25,6 +26,11 @@ public class SideLocation{
     public SideLocation(JsonNode sideLocation) {
         path = Path.of(sideLocation.get("filePath").toString());
         range = Range.closed(sideLocation.get("startLine").intValue(), sideLocation.get("endLine").intValue());
+    }
+
+    public SideLocation(String path, Range<Integer> range){
+        this.path = Path.of(path);
+        this.range = range;
     }
 
 }
