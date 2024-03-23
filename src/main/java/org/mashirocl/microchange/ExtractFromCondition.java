77b92c890f69f780dcd1f1ed340847b7b5777296
Lei Chen <mashirocl@gmail.com>
2024-03-23 12:57:23 +0100

    update: use a more accurate logic to detect

--- src/main/java/org/mashirocl/microchange/ExtractFromCondition.java
+++ src/main/java/org/mashirocl/microchange/ExtractFromCondition.java
@@ -3,6 +3,7 @@ package org.mashirocl.microchange;
 import com.github.gumtreediff.actions.model.Action;
 import com.github.gumtreediff.tree.Tree;
 import com.google.common.collect.Range;
+import lombok.extern.slf4j.Slf4j;
 import org.mashirocl.editscript.EditScriptStorer;
 import org.mashirocl.location.RangeOperations;
 import org.mashirocl.microchange.common.NodePosition;
@@ -14,6 +15,8 @@ import java.util.Map;
  * @author mashirocl@gmail.com
  * @since 2024/01/25 15:57
  */
+
+@Slf4j
 public class ExtractFromCondition implements MicroChangePattern{
     /**
      * is `move-tree`
@@ -26,40 +29,28 @@ public class ExtractFromCondition implements MicroChangePattern{
     @Override
     @Deprecated
     public boolean matchConditionGumTree(Action action, Map<Tree, Tree> mappings) {
-        return action.getName().equals("move-tree")
-                && action.getNode().getParent().getParent().getType().name.equals("IfStatement")
-                && mappings.containsKey(action.getNode())
-                && !mappings.get(action.getNode()).getParent().getType().name.equals("IfStatement")
-                && mappings.get(action.getNode()).getParent().getParent().getType().name.equals("Block");
+        return false;
     }
 
     @Override
     public boolean matchConditionGumTree(Action action, Map<Tree, Tree> mappings, Map<Tree, List<Action>> nodeActions) {
         if(!action.getName().equals("move-tree")) return false;
         Tree leftIfNode = NodePosition.isInIf(action.getNode());
-        if(leftIfNode!=null){
-            if(nodeActions.containsKey(leftIfNode) && nodeActions.get(leftIfNode).stream().anyMatch(a -> a.getName().equals("delete-tree")||a.getName().equals("delete-node"))) // the condition on the left side is removed
-            {
-                if(mappings.containsKey(action.getNode())){
-                    Tree movedExpression = mappings.get(action.getNode());
-                    Tree rightIfNode = NodePosition.isInIf(movedExpression);
-                    // the movedExpression is not in if
-                    if(rightIfNode==null){
-                        return true;
-                    }
-                    // Note the GumTree misIdentify the following case. To increase accuracy, not consider the following case
-                    // e.g. https://github.com/MashiroCl/my-refactoring-toy-example/commit/2bab7ef422f5a211224dc303d800068abe93e448: the !flag will be regarded as matched
-//                    else {     // the moved Expression is in if, but different with the original one
-//                        if(!mappings.containsKey(leftIfNode)){
-//                            return true;
-//                        }else {
-//                            return !mappings.get(leftIfNode).equals(rightIfNode);
-//                        }
-//                    }
 
-                }
+        if(leftIfNode!=null){
+            if(nodeActions.containsKey(leftIfNode)) log.info("nodeActions.get(leftIfNode) {}", nodeActions.get(leftIfNode));
+            // the condition on the left side is removed
+            // Note the GumTree misIdentify the following case. To increase accuracy, not consider the following case
+            // e.g. https://github.com/MashiroCl/my-refactoring-toy-example/commit/2bab7ef422f5a211224dc303d800068abe93e448: the !flag will be regarded as matched
+            //                    else {     // the moved Expression is in if, but different with the original one
+            //                        if(!mappings.containsKey(leftIfNode)){
+            //                            return true;
+            //                        }else {
+            //                            return !mappings.get(leftIfNode).equals(rightIfNode);
+            //                        }
+            //                    }
+            return nodeActions.containsKey(leftIfNode) && nodeActions.get(leftIfNode).stream().anyMatch(a -> a.getName().equals("delete-tree") || a.getName().equals("delete-node"));
             }
-        }
 
 
 //        Tree leftConditionExpressionNode = isInIfCondition(action.getNode());
@@ -114,24 +105,13 @@ public class ExtractFromCondition implements MicroChangePattern{
     public SrcDstRange getSrcDstRange(Action action, Map<Tree, Tree> mappings, Map<Tree, List<Action>> nodeActions, EditScriptStorer editScriptStorer) {
         SrcDstRange srcDstRange = new SrcDstRange();
         // left side
-        // the expression moved out from the if-block
-        Range<Integer> expression = RangeOperations.toLineRange(
-                RangeOperations.toRange(action.getNode()), editScriptStorer.getSrcCompilationUnit()
+        // the removed if block
+        Tree leftIfNode = NodePosition.isInIf(action.getNode());
+        Range<Integer> ifblock = RangeOperations.toLineRange(
+                RangeOperations.toRange(leftIfNode), editScriptStorer.getSrcCompilationUnit()
         );
 
-        srcDstRange.getSrcRange().add(expression);
-        // being removed if
-        if(nodeActions.containsKey(action.getNode().getParent())){
-            for(Action a:nodeActions.get(action.getNode().getParent())){
-                if(a.getName().equals("delete-node") || a.getName().equals("delete-tree")){
-                    srcDstRange.getSrcRange().add(
-                            RangeOperations.toLineRange(
-                                    RangeOperations.toRange(a.getNode()), editScriptStorer.getSrcCompilationUnit()
-                            )
-                    );
-                }
-            }
-        }
+        srcDstRange.getSrcRange().add(ifblock);
 
         // right side
         // the being moved expression, which used to be in the if-block
@@ -140,6 +120,7 @@ public class ExtractFromCondition implements MicroChangePattern{
                         RangeOperations.toRange(mappings.get(action.getNode())),editScriptStorer.getDstCompilationUnit()
                 )
         );
+
         return srcDstRange;
     }
 
