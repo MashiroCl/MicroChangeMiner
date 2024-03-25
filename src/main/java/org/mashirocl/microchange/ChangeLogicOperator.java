61f11ddc9f32eb97e5805f6beda46eb359c98eeb
Lei Chen <mashirocl@gmail.com>
2024-03-25 16:52:38 +0100

    feat: new type micro-change ChangeLogicOperator

---
+++ src/main/java/org/mashirocl/microchange/ChangeLogicOperator.java
@@ -0,0 +1,48 @@
+package org.mashirocl.microchange;
+
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.actions.model.Update;
+import com.github.gumtreediff.tree.Tree;
+import lombok.extern.slf4j.Slf4j;
+import org.mashirocl.editscript.EditScriptStorer;
+import org.mashirocl.location.RangeOperations;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * logic operator && is replaced by ||, or inverse
+ * @author mashirocl@gmail.com
+ * @since 2024/03/25 15:05
+ */
+@Slf4j
+public class ChangeLogicOperator implements MicroChangePattern{
+    @Override
+    public boolean matchConditionGumTree(Action action, Map<Tree, Tree> mappings) {
+        return false;
+    }
+
+    @Override
+    public boolean matchConditionGumTree(Action action, Map<Tree, Tree> mappings, Map<Tree, List<Action>> nodeActions) {
+        log.info("action {}", action);
+        log.info("treestring {}", action.getNode().toTreeString());
+        log.info("label {}", action.getNode().getLabel());
+        if(action.getName().equals("update-node")) log.info("value {}",((Update)action).getValue());
+        return action.getName().equals("update-node") && (action.getNode().getLabel().equals("&&") && ((Update)action).getValue().equals("||"))
+                || (action.getNode().getLabel().equals("||") && ((Update)action).getValue().equals("&&"));
+    }
+
+    @Override
+    public SrcDstRange getSrcDstRange(Action action, Map<Tree, Tree> mappings, Map<Tree, List<Action>> nodeActions, EditScriptStorer editScriptStorer) {
+        SrcDstRange srcDstRange = new SrcDstRange();
+        // left side
+        srcDstRange.getSrcRange().add(RangeOperations.toLineRange(RangeOperations.toRange(action.getNode()),
+                editScriptStorer.getSrcCompilationUnit()));
+        // right side
+        if(mappings.containsKey(action.getNode())) {
+            srcDstRange.getDstRange().add(RangeOperations.toLineRange(RangeOperations.toRange(mappings.get(action.getNode())),
+                    editScriptStorer.getDstCompilationUnit()));
+        }
+        return srcDstRange;
+    }
+}
