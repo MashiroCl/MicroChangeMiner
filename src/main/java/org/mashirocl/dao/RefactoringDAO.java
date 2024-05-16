92aca51d2ddc7c666c0583cc43661f41a0040748
Lei Chen <mashirocl@gmail.com>
2024-05-16 14:30:40 +0200

    feat: equals for RefactoringDAO

--- src/main/java/org/mashirocl/dao/RefactoringDAO.java
+++ src/main/java/org/mashirocl/dao/RefactoringDAO.java
@@ -1,16 +1,14 @@
 package org.mashirocl.dao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.mashirocl.refactoringminer.Refactoring;
 import org.mashirocl.refactoringminer.SideLocation;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author mashirocl@gmail.com
@@ -21,6 +19,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class RefactoringDAO {
     @JsonProperty("type")
     private String type;
@@ -38,5 +37,18 @@ public class RefactoringDAO {
         rightSideLocations = new LinkedList<>();
         refactoring.getLeftSideLocations().forEach(p->leftSideLocations.add(new SideLocationDAO(p)));
         refactoring.getRightSideLocations().forEach(p->rightSideLocations.add(new SideLocationDAO(p)));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RefactoringDAO refactoringDAO = (RefactoringDAO) o;
+        return Objects.equals(toString(), refactoringDAO.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(toString());
     }
 }
