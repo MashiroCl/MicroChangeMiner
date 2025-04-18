16926166bdf48f131931bd94be0d8c69c7bad0e2
Lei Chen <mashirocl@gmail.com>
2025-04-18 20:25:55 +0900

    feat: designed detection rule for ConvertWhileToFor

---
+++ src/main/java/org/mashirocl/microchange/loop/ConvertWhileToFor.java
@@ -0,0 +1,48 @@
+package org.mashirocl.microchange.loop;
+
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.tree.Tree;
+import org.mashirocl.editscript.EditScriptStorer;
+import org.mashirocl.location.RangeOperations;
+import org.mashirocl.microchange.MicroChangePattern;
+import org.mashirocl.microchange.SrcDstRange;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author mashirocl@gmail.com
+ * @since 2025/04/18 20:22
+ */
+public class ConvertWhileToFor implements MicroChangePattern {
+    /**
+     * The while-loop is replaced by a for-loop
+     *
+     * The A child node (while-body) is moved from a while-statement to be the child of a for-statement,
+     * the original while-statement is removed
+     */
+    @Override
+    public boolean matchConditionGumTree(Action action, Map<Tree, Tree> mappings) {
+        return false;
+    }
+
+    @Override
+    public boolean matchConditionGumTree(Action action, Map<Tree, Tree> mappings, Map<Tree, List<Action>> nodeActions) {
+        return action.getName().equals("move-tree")
+                && action.getNode().getParent().getType().name.equals("WhileStatement")
+                && nodeActions.containsKey(action.getNode())
+                && nodeActions.get(action.getNode().getParent()).stream().anyMatch(p->p.getName().contains("delete"))
+                && mappings.containsKey(action.getNode())
+                && mappings.get(action.getNode()).getParent().getType().name.equals("ForStatement");
+    }
+
+    @Override
+    public SrcDstRange getSrcDstRange(Action action, Map<Tree, Tree> mappings, Map<Tree, List<Action>> nodeActions, EditScriptStorer editScriptStorer) {
+        // being moved for-loop body
+        // TODO: should we include the for-statement line?
+        SrcDstRange srcDstRange = new SrcDstRange();
+        srcDstRange.getDstRange().add(RangeOperations.toLineRange(RangeOperations.toRange(action.getNode()),
+                editScriptStorer.getDstCompilationUnit()));
+        return srcDstRange;
+    }
+}
