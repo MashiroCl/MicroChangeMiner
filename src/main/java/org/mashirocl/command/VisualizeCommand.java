99b4ce46208d72248727115e64ed59050723950b
Lei Chen <mashirocl@gmail.com>
2025-09-19 20:25:23 +0900

    feat: commands for processing in visualization

--- src/main/java/org/mashirocl/command/VisualizeCommand.java
+++ src/main/java/org/mashirocl/command/VisualizeCommand.java
@@ -1,7 +1,16 @@
 package org.mashirocl.command;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mashirocl.visualize.Collector;
+import org.mashirocl.visualize.UtilCommit;
 import picocli.CommandLine;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import static org.mashirocl.visualize.Collector.*;
 
 /**
  * @author mashirocl@gmail.com
@@ -9,17 +18,35 @@ import picocli.CommandLine;
  */
 
 @Slf4j
-@CommandLine.Command(name = "visualize", description = "Process related to visualize micro-changes")
-public class VisualizeCommand {
+@CommandLine.Command(name = "visualize", description = "Use the mined result to visualize different types code changes")
+public class VisualizeCommand implements Callable<Integer> {
     public static class Config {
-        @CommandLine.Parameters(index = "0", description = "")
-        String refactoringMinerPath;
+        @CommandLine.Parameters(index = "0", description = "The path to the mined micro-change json file, which is the output of micro-change miner")
+        String microChangeJsonPath;
 
-        @CommandLine.Parameters(index = "1", description = "")
-        String repositoryPath;
+        @CommandLine.Parameters(index = "1", description = "The directory to the mined refactoring, which is the output of micro-change miner")
+        String refactoringDirectory;
 
-        @CommandLine.Parameters(index = "2", description = "")
+        @CommandLine.Parameters(index = "2", description = "The path to the file-level repository git")
+        String fileLevelGitPath;
+
+        @CommandLine.Parameters(index = "3", description = "The json file path for output")
         String outputPath;
 
+    }
+
+    @CommandLine.Mixin
+    protected VisualizeCommand.Config config = new VisualizeCommand.Config();
+
+    @Override
+    public Integer call() throws Exception{
+        // Conllect the mining result
+        HashMap<String, UtilCommit> res = collectMCfromJson(config.microChangeJsonPath, config.refactoringDirectory);
+        List<UtilCommit> commits = new LinkedList<>();
+        res.keySet().forEach(p->commits.add(res.get(p)));
+
+        calcUtilDiff(commits, config.fileLevelGitPath);
+        commitsToSimplifiedUtilJson(commits, "/Users/leichen/project/semantic_lifter/visualize/mined_microchanges/mbassador.json");
+        return  0;
     }
 }
